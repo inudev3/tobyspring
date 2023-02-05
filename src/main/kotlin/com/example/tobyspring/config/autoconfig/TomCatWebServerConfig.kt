f0649bc957ae87345e124ffd18a11e1878702e30
Inu Jung <inust33@gmail.com>
2023-02-06 00:35:41 +0900

    ..

--- src/main/kotlin/com/example/tobyspring/config/autoconfig/TomCatWebServerConfig.kt
+++ src/main/kotlin/com/example/tobyspring/config/autoconfig/TomCatWebServerConfig.kt
@@ -2,6 +2,9 @@ package com.example.tobyspring.config.autoconfig
 
 import com.example.tobyspring.config.annotation.ConditionalOnMyClass
 import com.example.tobyspring.config.annotation.MyAutoConfiguration
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
@@ -10,6 +13,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Condition
 import org.springframework.context.annotation.ConditionContext
 import org.springframework.context.annotation.Conditional
+import org.springframework.context.annotation.Import
 import org.springframework.core.env.Environment
 import org.springframework.core.type.AnnotatedTypeMetadata
 import org.springframework.util.ClassUtils
@@ -17,13 +21,17 @@ import org.springframework.util.ClassUtils
 
 @MyAutoConfiguration
 @ConditionalOnMyClass("org.apache.catalina.startup.Tomcat")
-class TomCatWebServerConfig{
+@Import(ServerProperties::class)
+class TomCatWebServerConfig( @Value("\${contextPath}")val value:String){
 
+//    val log= LoggerFactory.getLogger(TomCatWebServerConfig::class.java)
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
     @ConditionalOnProperty(prefix = "server", name = arrayOf("type"), havingValue = "tomcat")
-    fun servletWebServerFactory(env:Environment): ServletWebServerFactory = TomcatServletWebServerFactory().also{
-        it.contextPath=env.getProperty("contextPath")
+    fun servletWebServerFactory(serverProperties: ServerProperties): ServletWebServerFactory = TomcatServletWebServerFactory().also{
+
+        it.contextPath=serverProperties.contextPath
+        it.port=serverProperties.port
     }
 
 }
