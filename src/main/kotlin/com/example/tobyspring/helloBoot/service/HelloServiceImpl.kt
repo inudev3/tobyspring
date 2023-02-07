14b2b7472d5fdd48dc821128d485f5164affcb8a
Inu Jung <inust33@gmail.com>
2023-02-07 10:45:51 +0900

    repository test

--- src/main/kotlin/com/example/tobyspring/helloBoot/service/HelloServiceImpl.kt
+++ src/main/kotlin/com/example/tobyspring/helloBoot/service/HelloServiceImpl.kt
@@ -1,9 +1,14 @@
 package com.example.tobyspring.helloBoot.service
 
 
+import com.example.tobyspring.helloBoot.repository.HelloRepository
 import org.springframework.stereotype.Service
 
 @Service
-class HelloServiceImpl : HelloService {
-    override fun sayHello(name: String)= "hello $name"
+class HelloServiceImpl (private val helloRepository: HelloRepository): HelloService {
+
+    override fun sayHello(name: String)= kotlin.run {
+        helloRepository.increaseCount(name)
+        "hello $name"
+    }
 }
